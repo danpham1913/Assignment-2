69b8201207c8eb934df90d589dc293fe42faadf7
danpham1913 <125883975+danpham1913@users.noreply.github.com>
2023-10-20 18:09:24 +1100

    Add files via upload

--- @densoVP6242/densoVP6242.m
+++ @densoVP6242/densoVP6242.m
@@ -34,7 +34,7 @@ function self = densoVP6242(baseTr,useTool,toolFilename)
             link(1) = Link('d',0.28,'a',0,'alpha',pi/2,'offset',0, 'qlim',[-pi pi]) ;
             link(2) = Link('d',0,'a',0.210,'alpha',0,'offset',pi/2, 'qlim',[-pi pi]) ;
             link(3) = Link('d',0,'a',0.233,'alpha',0,'offset',-deg2rad(70.33), 'qlim',[-pi pi]) ;
-            link(4) = Link('d',0,'a',0.07,'alpha',pi/2,'offset',-deg2rad(90-70.33), 'qlim',[-pi pi]);       
+            link(4) = Link('d',0,'a',0.18,'alpha',pi/2,'offset',-deg2rad(90-70.33), 'qlim',[-pi pi]);       
             self.model = SerialLink(link,'name',self.name);
         end
      
