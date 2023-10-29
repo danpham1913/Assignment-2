ac229dffb8ba733a6aeb67fa3b69180524c5573e
danpham1913 <125883975+danpham1913@users.noreply.github.com>
2023-10-29 21:17:21 +1100

    Add files via upload

--- Assignment1.m
+++ Assignment1.m
@@ -7,55 +7,60 @@ clear
 Environment
 
 %Robot LinearUR3
-robot = densoVP6242(transl(1.5,0,0.8));
+robot = densoVP6242(transl(-0.25,-0.45,0.75));
 robot_q = robot.model.getpos;
 % robot.model.animate(robot_q);
 initial_ee = robot.model.fkine(robot_q);
-% robot2 = densoVP6242(transl(1.5,0,0.8));
-
+robot2 = UR3(transl(-0.2,0.25,0.8));
+robot2_q = robot2.model.getpos;
 
 %Placing Bricks
-brick1 = PlaceObject('HalfSizedRedGreenBrick.ply',[1.25 0 0.85]);
-vertsb1 = get(brick1,'Vertices');
+passport = PlaceObject('passport_ply.PLY',[0.20 0.25 1.21]);
+vertspp1 = get(passport,'Vertices');
 % set(brick1,'Vertices',vertsb1(:,1:3))
 
-% b1_ee0 = transl(0.52,0,0.85);
-% b1_eeF = transl(2,0,1);
-qGuest = [0 0 0 0 0 0 0];
-qb1 = robot.model.ikcon(transl(1.25,0,0.85) * troty(pi));
-qb2 = robot.model.ikcon(transl(1.5,-0.5,0.85)* troty(pi));
+%UR3 movement path
+
+% b1_ee0 = transl(0.20,0.25,1.21);
+% b1_eeF = transl(-0.4,0.15,0.8);
+qb1 = robot2.model.ikcon(transl(0.20,0.25,1.21) * troty(pi));
+qb2 = robot2.model.ikcon(transl(-0.20,0.25,1.21)*troty(pi));
+
+
+qb_end = robot2.model.ikcon(transl(-1.5,-0.5,0.8)* troty(pi));
+
 
 steps = 50;
 
-q_1 = jtraj(robot_q,qb1,steps); %initial to brick
+q_1 = jtraj(robot2_q,qb1,steps); %initial to brick
 q_2 = jtraj(qb1,qb2,steps); %brick to end brick
-q_3 = jtraj(qb2,robot_q,steps); %end brick back to initial
+q_3 = jtraj(qb2,robot2_q,steps); %end brick back to initial
 
 
 for j = 1:size(q_1,1)                                             % animate trajectory
-    robot.model.animate(q_1(j,:));
-    tr = robot.model.fkine(q_1(j,:)).T;
+    robot2.model.animate(q_1(j,:));
+    tr = robot2.model.fkine(q_1(j,:)).T;
 %     transformVertBrick1 = [vertsb1, ones(size(vertsb1,1),1)]*tr';
 %     set(brick1,'Vertices',transformVertBrick1(:,1:3));
     drawnow(); 
 end
 
-trfix = inv(robot.model.fkine(robot.model.getpos).T) * [vertsb1, ones(size(vertsb1,1),1)]';
+trfix = inv(robot2.model.fkine(robot2.model.getpos).T) * [vertspp1, ones(size(vertspp1,1),1)]';
 
 
 for j = 1:size(q_2,1)                                             % animate trajectory
-    robot.model.animate(q_2(j,:));
+    robot2.model.animate(q_2(j,:));
 %     tr = robot.model.fkine(q_2(j,:)).T;
 %     transformVertBrick1 = (tr * [vertsb1, ones(size(vertsb1,1),1)]')';
 %     set(brick1,'Vertices',transformVertBrick1(:,1:3));
-    transbrick = robot.model.fkine(q_2(j,:)).T * trfix;
-    trvert = transbrick(1:3,:)';
-    set(brick1,'Vertices',trvert);
+    updatepoint = robot2.model.fkine(q_2(j,:)).T * trfix;
+    trvert = updatepoint(1:3,:)';
+    set(passport,'Vertices',trvert);
     drawnow(); 
 end
 
 for j = 1:size(q_3,1)                                             % animate trajectory
-    robot.model.animate(q_3(j,:));
+    robot2.model.animate(q_3(j,:));
 %     tr = robot.model.fkine(q_2(j,:)).T;
 %     transformVertBrick1 = [vertsb1, ones(size(vertsb1,1),1)]*tr';
 %     set(brick1,'Vertices',transformVertBrick1(:,1:3));
--- Environment.m
+++ Environment.m
@@ -17,11 +17,11 @@ grid on;
 % surf([4,4;4,4],[0.9,1.4;0.9,1.4],[1,1;1.5,1.5],'CData',imread('Warning.jpg'),'FaceColor','texturemap');
 
 %Add objects
-PlaceObject('tableBrown2.1x1.4x0.5m.ply',[1 0 0])
+% PlaceObject('tableBrown2.1x1.4x0.5m.ply',[1 0 0])
 % PlaceObject('fenceAssemblyGreenRectangle4x8x2.5m.ply')
 % PlaceObject('Worker.ply',[2 3 0])
 % PlaceObject('personFemaleBusiness.ply',[1 3 0])
-% PlaceObject('fullroom_ply.ply',[0 0 1])
+ PlaceObject('fullroom_ply.ply',[0 0 1])
 % PlaceObject('stampper_ply.ply',[0 0 0.8])
 camlight
 
