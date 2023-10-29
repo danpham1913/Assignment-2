29ee5fb8a0b1859ad0936786460156c688c5860d
11980099 <Kent.tran@student.uts.edu.au>
2023-10-30 09:11:15 +1100

    Working on Main

--- PSRFunctions.m
+++ PSRFunctions.m
@@ -5,14 +5,8 @@ classdef PSRFunctions
     end
 
     methods (Static)
-        %% UR3Task
-        % Brief: Function to execute UR3 movements (pick and place and report back when complete.
-        % Inputs: Stamped   - status if passport has been stamped.
-        % Output: Status    - 0:Movement incomplete
-        %                   - 1:Passport picked up and moved to stamping
-        %                       location completed.
-        %                   - 2:passport moved back to original location
-        %                       and drop off complete.
+        %% Environment
+        % Brief: Function to load environment
         function Environment()
             hold on
             %Set up workspace
@@ -23,6 +17,30 @@ classdef PSRFunctions
             grid on;
             PlaceObject('fullroom_ply.ply',[0.175 0.1 1]);
             camlight
+        end
+        %% UR3Task
+        % Brief: Move Robot Model and Object from start to end
+        % Inputs: - qStart: Starting q angles
+        %         - qEnd: Enfing q angles
+        %         - Robot: Robot to be manipulated
+        % Output: - Trajectory: array to of q angles get from qStart to qEnd
+        %         - CurrentStep: Current Step performed by Trajectory
+        function [trajectory,currentStep] = moveTo(qStart,qEnd,Robot)
+            Start = Robot.model.fkineUTS(qStart);
+            End = Robot.model.fkineUTS(qEnd);
+            StartTr = Start(1:3,4);
+            EndTr = End(1:3,4);
+            travelDistance = norm(StartTr-EndTr)
+            steps = round(100*travelDistance+15);
+
+            trajectory = jtraj(qStart,qEnd,steps);
+
+            for j = 1:size(trajectory,1)                                             % animate trajectory
+                Robot.model.animate(trajectory(j,:));
+                currentStep = j;
+                drawnow;
+                pause(0.1);
+            end
         end
     end
 end
--- main.m
+++ main.m
@@ -6,18 +6,18 @@
 function [] = main()
 %% Load Environment
 PSRFunctions.Environment;
-%% Load Robots
+% % Load Robots
 %Load UR3
 UR3Robot = UR3(transl(0,0.3,0.7));
 %Load VP6242
 VP6242 = densoVP6242(transl(0,-0.3,0.6)*trotz(-pi/2));
-
-%% Load Objects
+VP6242.model.teach();% Open a menu to move the robot manually
+% % Load Objects
 %Load Passport
-passport = PlaceObject('passport_ply.PLY',[0.15 0.22 1.00]);
+passport = PlaceObject('passport_ply.PLY',[0.325 0.3125 1]);
 passportverts = get(passport,'Vertices');
 %Load Stamp
-stamp = PlaceObject('passport_ply.PLY',[0.15 0.22 1.00]);
+stamp = PlaceObject('stampper_ply.PLY',[0.1315 -0.02 0.707]);
 stampVerts = get(passport,'Vertices');
 
 %% Establish Flags
@@ -34,14 +34,31 @@ stampVerts = get(passport,'Vertices');
     %SafetyFunctions.DoorOpen
 
 %% Perform Tasks
-robot_q = robot.model.getpos;
-initial_ee = robot.model.fkine(robot_q);
+robot_q = VP6242.model.getpos;
+initial_ee = VP6242.model.fkine(robot_q);
 
-robot2_q = robot2.model.getpos;
+robot2_q = UR3Robot.model.getpos;
 % Passport moved to start location
 %% UR3 Movement 1
+trfix = inv(UR3Robot.model.fkine(UR3Robot.model.getpos).T) * [passportverts, ones(size(passportverts,1),1)]';
 % UR3 Pick Up Passport
+
 % UR3 Move to stamp location
+qVPoriginal = [0 0 0 0 0 0 0 0]; %VP6242 Original Position
+StampStart= transl([0.1315 -0.02 0.71])*rpy2tr(-180,0,-180,'deg');
+StampEnd= transl([-0.14,0,0.72])*rpy2tr(-180,0,-180,'deg');% get transform of current target brick
+
+qstampStart = VP6242.model.ikcon(StampStart);
+qstampStartLift= VP6242.model.ikcon(StampStart*transl([0,0,-0.05]));
+qstampEnd = VP6242.model.ikcon(StampEnd);
+qstampEndLift = VP6242.model.ikcon(StampEnd*transl([0,0,-0.05]));
+
+[moveVP1,CurrentStep] = PSRFunctions.moveTo(qstampStart,qstampStartLift,VP6242);
+[moveVP2,CurrentStep] = PSRFunctions.moveTo(qstampStartLift,qstampEndLift,VP6242);
+[moveVP3,CurrentStep] = PSRFunctions.moveTo(qstampEndLift,qstampEnd,VP6242);
+[moveVP4,CurrentStep] = PSRFunctions.moveTo(qstampEnd,qstampEndLift,VP6242);
+[moveVP5,CurrentStep] = PSRFunctions.moveTo(qstampEndLift,qstampStartLift,VP6242);
+[moveVP6,CurrentStep] = PSRFunctions.moveTo(qstampStartLift,qstampStart,VP6242);
 %% VP6242 Movement 2
 % VP6242 pick up stamp,
 end
