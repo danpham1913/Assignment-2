cf6ae71503644314f9b59736ef9ba101813816b0
danpham1913 <125883975+danpham1913@users.noreply.github.com>
2023-11-03 02:42:08 +1100

    Add files via upload

--- Environment.m
+++ Environment.m
@@ -3,7 +3,7 @@ function Environment()
 hold on
 
 %Set up workspace
-axis ([-1 1.3 -1 1 0 2.5]);
+axis ([-2 4 -1.5 1.5 0 3])
 axis equal;  
 xlabel ('X');
 ylabel ('Y');
@@ -11,17 +11,23 @@ zlabel ('Z');
 grid on;
 
 %Set up walls
-% surf([-5,-5;5,5],[-5,5;-5,5],[0.01,0.01;0.01,0.01],'CData',imread('BestConcrete.jpg'),'FaceColor','texturemap');
-% surf([-5,5;-5,5],[4,4;4,4],[3,3;0.01,0.01],'CData',imread('ConstructionSite.jpg'),'FaceColor','texturemap');
-% surf([-5,-5;-5,-5],[4,-4;4,-4],[3,3;0.01,0.01],'CData',imread('ConstructionSite.jpg'),'FaceColor','texturemap');
-% surf([4,4;4,4],[0.9,1.4;0.9,1.4],[1,1;1.5,1.5],'CData',imread('Warning.jpg'),'FaceColor','texturemap');
+surf([-2,-2;4,4],[-1.5,1.5;-1.5,1.5],[0.01,0.01;0.01,0.01],'CData',imread('floor.jpg'),'FaceColor','texturemap');
+surf([-2,4;-2,4],[-1.5,-1.5;-1.5,-1.5],[3,3;0.01,0.01],'CData',imread('Screenshot(2).jpg'),'FaceColor','texturemap');
+surf([4,4;4,4],[-1.5,1.5;-1.5,1.5],[3,3;0.01,0.01],'CData',imread('Screenshot(3).jpg'),'FaceColor','texturemap');
+surf([-0.6,-0.4;-0.6,-0.4],[-0.85,-0.85;-0.85,-0.85],[1,1;1.2,1.2],'CData',imread('Warning.jpg'),'FaceColor','texturemap');
 
 %Add objects
 % PlaceObject('tableBrown2.1x1.4x0.5m.ply',[1 0 0])
 % PlaceObject('fenceAssemblyGreenRectangle4x8x2.5m.ply')
-% PlaceObject('Worker.ply',[2 3 0])
-PlaceObject('personFemaleBusiness.ply',[0.75 0 0]);
- PlaceObject('ColoredRoom.ply',[0 0 1]);
-% PlaceObject('stampper_ply.ply',[0 0 0.8])
+PlaceObject('personMaleCasual.ply',[0.85 0 0])
+PlaceObject('personFemaleBusiness.ply',[1.5 0 0])
+PlaceObject('fullroom2_ply (1).PLY',[0 0 1])
+PlaceObject('door.PLY',[0 0 1])
+PlaceObject('personMaleOld.ply',[2.25 0 0])
+PlaceObject('suitcase.ply',[2.25 0 0])
+PlaceObject('suitcase2.ply',[0.85 0.25 0])
+PlaceObject('suitcase.ply',[1.5 1 0])
+PlaceObject('fireExtinguisher.ply',[0 0.8 0])
+PlaceObject('emergencyStopButton.ply',[-0.8 -0.8 1])
 camlight
 
